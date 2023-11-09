84e40fc28fa609888d0823ef6ed49b6d46f37565
LordAlister <khalef.sofiane91@gmail.com>
2023-11-09 19:36:13 +0100

    add alert modal

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -11,6 +11,10 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useState } from 'react';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import toast from 'react-hot-toast';
+import axios from 'axios';
+import { useParams, useRouter } from 'next/navigation';
+import { AlertModal } from '@/components/modals/alert-modal';
 
 interface SettingsFormProps {
     initialData : Store;
@@ -25,6 +29,8 @@ type SettingsFormValues = z.infer<typeof formSchema>;
 export const SettingsForm: React.FC<SettingsFormProps> = ({
     initialData
 }) =>{
+    const params = useParams()
+    const router = useRouter()
     const[open,setOpen] = useState(false);
     const [loading,setLoading] = useState(false);
 
@@ -34,10 +40,39 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
     });
 
     const onSubmit = async(data: SettingsFormValues)=>{
-        console.log(data)
+        try {
+            setLoading(true)
+            await axios.patch(`/api/stores/${params.storeId}`,data);
+            router.refresh();
+            toast.success("Store updated")
+        } catch (error) {
+            toast.error("Something went wrong")
+        } finally{
+            setLoading(false);
+        }
     };
+    const onDelete = async () => {
+        try {
+            setLoading(true)
+            await axios.delete(`/api/stores/${params.storeId}`);
+            router.refresh();
+            router.push('/');
+            toast.success("Store deleted");
+        } catch (error) {
+            toast.error("Make sure you removed all products and categories first.")
+        } finally{
+            setLoading(false);
+            setOpen(false)
+        }
+    }
     return(
         <>
+        <AlertModal
+        isOpen={open}
+        onClose={()=> setOpen(false)}
+        onConfirm={()=> onDelete()}
+        loading={loading}
+        />
             <div className="flex items-center justify-between">
                 <Heading
                 title="Settings"
