49d303a7ddfd0e967680277bd080d1eb3ed6b657
LordAlister <khalef.sofiane91@gmail.com>
2023-11-12 15:24:08 +0100

    Billboard create and update

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -15,8 +15,7 @@ import toast from 'react-hot-toast';
 import axios from 'axios';
 import { useParams, useRouter } from 'next/navigation';
 import { AlertModal } from '@/components/modals/alert-modal';
-import { ApiAlert } from '@/components/ui/api-alert';
-import { useOrigin } from '@/hooks/use-origin';
+import ImageUpload from '@/components/ui/image-upload';
 
 interface BillboardFormProps {
     initialData : Billboard | null;
@@ -34,7 +33,6 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
 }) =>{
     const params = useParams()
     const router = useRouter()
-    const origin = useOrigin()
     
     const[open,setOpen] = useState(false);
     const [loading,setLoading] = useState(false);
@@ -55,9 +53,14 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
     const onSubmit = async(data: BillboardFormValues)=>{
         try {
             setLoading(true)
-            await axios.patch(`/api/stores/${params.storeId}`,data);
+            if(initialData){
+                await axios.patch(`/api/${params.storeId}/billboards/${params.billboardId}`,data);
+            }else{
+                await axios.post(`/api/${params.storeId}/billboards`,data);
+            }
             router.refresh();
-            toast.success("Store updated")
+            router.push(`/${params.storeId}//billboards`)
+            toast.success(toastMessage)
         } catch (error) {
             toast.error("Something went wrong")
         } finally{
@@ -67,12 +70,12 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
     const onDelete = async () => {
         try {
             setLoading(true)
-            await axios.delete(`/api/stores/${params.storeId}`);
+            await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`);
             router.refresh();
             router.push('/');
-            toast.success("Store deleted");
+            toast.success("Billboard deleted.");
         } catch (error) {
-            toast.error("Make sure you removed all products and categories first.")
+            toast.error("Make sure you removed all categories using this billboard first.")
         } finally{
             setLoading(false);
             setOpen(false)
@@ -105,6 +108,24 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
             <Separator />
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8 w-full'>
+                    <FormField 
+                        control={form.control}
+                        name = "imageUrl"
+                        render={({ field}) =>(
+                            <FormItem>
+                                <FormLabel>Background image</FormLabel>
+                                <FormControl>
+                                    <ImageUpload 
+                                    value={field.value ? [field.value] : []}
+                                    disabled={loading}
+                                    onChange={(url)=>field.onChange(url)}
+                                    onRemove={()=>field.onChange("")}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
                     <div className='grid grid-cols-3 gap-8'>
                         <FormField 
                         control={form.control}
